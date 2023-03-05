4f0e5d776fea5f0df9606b48608e445ba244a02d
MauR27 <antonio27mauricio27@gmail.com>
2023-03-05 13:51:41 +0100

    le di dise;o y un poco de funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useInterval } from "usehooks-ts";
 
 function App() {
+  let count = 5;
+  let play;
+  const [input, setInput] = useState(count);
+  const [counter, setCounter] = useState("00");
+
+  useEffect(() => {
+    if (counter > 0) {
+      play = setTimeout(() => setCounter(counter - 1), 1000);
+    } else {
+      setCounter(59);
+      setInput(input - 1);
+    }
+  }, [counter]);
+
+  function clickPlay() {
+    // clearTimeout(play);
+  }
+  function clickPause() {}
+
+  function onWheelUp() {
+    if (input < 60) {
+      setInput(input + 1);
+    }
+  }
+
+  function onWheelDown() {
+    if (input > 0) {
+      setInput(input - 1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="belt-up"></div>
+      <div id="display-box">
+        <p>Session Time</p>
+        <div className="session-time">
+          <div onWheel={onWheelUp}>▲</div>
+          <div>{input}</div>
+          <div onWheel={onWheelDown}>▼</div>
+        </div>
+        <div className="clock-time">
+          <h1>
+            {input} : {counter}
+          </h1>
+          <button onClick={clickPlay}>P</button>
+          <button onClick={clickPause}>R</button>
+        </div>
+        <div className="break-time">
+          <div onWheel={onWheelUp}>▲</div>
+          <div>{input}</div>
+          <div onWheel={onWheelDown}>▼</div>
+        </div>
+        <p>Break Time</p>
+      </div>
+      <div id="belt-down">
+        <div className="dot">.</div>
+        <div>.</div>
+        <div>.</div>
+      </div>
     </div>
   );
 }
