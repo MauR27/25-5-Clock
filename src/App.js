8f4dc28adbf0e9276bd39af1b38882106a282333
MauR27 <antonio27mauricio27@gmail.com>
2023-03-06 10:28:01 +0100

    terminado, funcionalidad y diseno

--- src/App.js
+++ src/App.js
@@ -1,62 +1,132 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { useInterval } from "usehooks-ts";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import {
+  MdRestartAlt,
+  MdOutlineKeyboardDoubleArrowUp,
+  MdOutlineKeyboardDoubleArrowDown,
+} from "react-icons/md";
 
 function App() {
-  let count = 5;
-  let play;
+  let count = 25;
+  const [changeSession, setChangeSession] = useState(false);
+  const [sessionCount, setSessionCount] = useState(count);
+  const [counter, setCounter] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const [breakInput, setBreakInput] = useState(5);
   const [input, setInput] = useState(count);
-  const [counter, setCounter] = useState("00");
+
+  useInterval(
+    () => {
+      if (counter > 0) {
+        setCounter((counter) => counter - 1);
+      } else {
+        setCounter(59);
+        setSessionCount((sessionCount) => sessionCount - 1);
+      }
+    },
+    isPaused ? 1000 : null
+  );
 
   useEffect(() => {
-    if (counter > 0) {
-      play = setTimeout(() => setCounter(counter - 1), 1000);
-    } else {
-      setCounter(59);
-      setInput(input - 1);
+    if (sessionCount === 0 && counter === 0) {
+      alarmPlay();
+      setIsPaused(false);
+      setTimeout(() => {
+        setChangeSession((prev) => !prev);
+        setSessionCount(!changeSession ? breakInput : input);
+        setIsPaused(true);
+      }, 3000);
     }
   }, [counter]);
 
-  function clickPlay() {
-    // clearTimeout(play);
+  function clickPause() {
+    setIsPaused((prev) => !prev);
   }
-  function clickPause() {}
 
-  function onWheelUp() {
-    if (input < 60) {
+  function inputWheelUp() {
+    if (sessionCount < 60) {
+      setSessionCount(sessionCount + 1);
       setInput(input + 1);
+      setSessionCount(input + 1);
+      setCounter(0);
     }
   }
 
-  function onWheelDown() {
-    if (input > 0) {
+  function inputWheelDown() {
+    if (sessionCount > 1) {
+      setSessionCount(sessionCount - 1);
       setInput(input - 1);
+      setSessionCount(input - 1);
+      setCounter(0);
     }
+  }
+
+  function breakWheelUp() {
+    if (breakInput < 60) {
+      setBreakInput(breakInput + 1);
+    }
+  }
+  function breakWheelDown() {
+    if (breakInput > 1) {
+      setBreakInput(breakInput - 1);
+    }
+  }
+
+  function alarmPlay() {
+    const onPlay = document.getElementById("alarm");
+    onPlay.play();
+  }
+
+  function restarClock() {
+    setSessionCount(25);
+    setInput(25);
+    setBreakInput(5);
+    setCounter(0);
+    setIsPaused(false);
   }
 
   return (
     <div className="App">
+      <audio src="sounds/alarm-2.mp3" id="alarm"></audio>
       <div id="belt-up"></div>
       <div id="display-box">
-        <p>Session Time</p>
+        <p>Session Length</p>
         <div className="session-time">
-          <div onWheel={onWheelUp}>▲</div>
-          <div>{input}</div>
-          <div onWheel={onWheelDown}>▼</div>
+          <div onWheel={!isPaused && inputWheelUp}>
+            <MdOutlineKeyboardDoubleArrowUp />
+          </div>
+          <h3>{input}</h3>
+          <div onWheel={!isPaused && inputWheelDown}>
+            <MdOutlineKeyboardDoubleArrowDown />
+          </div>
         </div>
         <div className="clock-time">
+          <h4>{!changeSession ? "Session" : "Break"}</h4>
           <h1>
-            {input} : {counter}
+            {sessionCount}:{counter < 10 ? "0" + counter : counter}
           </h1>
-          <button onClick={clickPlay}>P</button>
-          <button onClick={clickPause}>R</button>
+          <button
+            onClick={clickPause}
+            disabled={sessionCount === 0 && counter === 0 ? true : false}
+          >
+            {!isPaused ? <BsPlayFill /> : <BsPauseFill />}
+          </button>
+          <button onClick={restarClock}>
+            <MdRestartAlt />
+          </button>
         </div>
         <div className="break-time">
-          <div onWheel={onWheelUp}>▲</div>
-          <div>{input}</div>
-          <div onWheel={onWheelDown}>▼</div>
+          <div onWheel={!isPaused && breakWheelUp}>
+            <MdOutlineKeyboardDoubleArrowUp />
+          </div>
+          <h3>{breakInput}</h3>
+          <div onWheel={!isPaused && breakWheelDown}>
+            <MdOutlineKeyboardDoubleArrowDown />
+          </div>
         </div>
-        <p>Break Time</p>
+        <p>Break Length</p>
       </div>
       <div id="belt-down">
         <div className="dot">.</div>
